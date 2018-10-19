01b3fb3b62c78c3afe6d6bee4cb4d93e4ae71af4
vini23hot <vini23hot@gmail.com>
2018-10-19 19:01:04 -0300

    Implemented the personal website

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('section').forEach(section => {
+  section.addEventListener('mouseenter', function() {
+      this.firstElementChild.classList.add('section-hover')
+  }, false)
+  section.addEventListener('mouseleave', function() {
+      this.firstElementChild.classList.remove('section-hover')
+  }, false)
+})
